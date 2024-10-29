a9d4d835e2f3b5a32c6cf87576753ee16e828c6a
Azeez1 <sabaazeez12@gmail.com>
2024-10-29 19:24:54 +0000

    lambda functions

---
+++ aws-cloud-resume/TerraForm Files/lambda_function.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+from decimal import Decimal
+
+  def lambda_handler(event, context):
+      dynamodb = boto3.resource('dynamodb')
+      table = dynamodb.Table('Visitors')
+
+      # Update visitor count
+      response = table.update_item(
+                  Key={'id': 'visit_count'},
+                          UpdateExpression='ADD visitCount :incr',
+                                  ExpressionAttributeValues={':incr': 1},
+                                          ReturnValues="UPDATED_NEW"
+                                              )
+
+      # Convert Decimal to int before returning
+      visit_count = int(response['Attributes']['visitCount'])  # Convert to int
+
+      return {
+                  'statusCode': 200,
+                          'body': json.dumps({'visitCount': visit_count})  # Use the converted visit_count
+                              }
+
