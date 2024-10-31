620ff63a12fa830159c666b67897115ef0b19c94
Azeez1 <sabaazeez12@gmail.com>
2024-10-31 10:41:27 -0400

    Update lambda_function.py

--- aws-cloud-resume/TerraForm Files/lambda_function.py
+++ aws-cloud-resume/TerraForm Files/lambda_function.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Visitors202')
+    table = dynamodb.Table('Visitors2024')
 
     # Update visitor count
     response = table.update_item(
