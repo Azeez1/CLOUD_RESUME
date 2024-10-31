0a0181f5e4a0c76172c826fccfac39fb7da84854
Azeez1 <sabaazeez12@gmail.com>
2024-10-31 10:41:01 -0400

    Update lambda_function.py

--- aws-cloud-resume/TerraForm Files/lambda_function.py
+++ aws-cloud-resume/TerraForm Files/lambda_function.py
@@ -2,23 +2,22 @@ import json
 import boto3
 from decimal import Decimal
 
-  def lambda_handler(event, context):
-      dynamodb = boto3.resource('dynamodb')
-      table = dynamodb.Table('Visitors')
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Visitors202')
 
-      # Update visitor count
-      response = table.update_item(
-                  Key={'id': 'visit_count'},
-                          UpdateExpression='ADD visitCount :incr',
-                                  ExpressionAttributeValues={':incr': 1},
-                                          ReturnValues="UPDATED_NEW"
-                                              )
+    # Update visitor count
+    response = table.update_item(
+        Key={'id': 'visit_count'},
+        UpdateExpression='ADD visitCount :incr',
+        ExpressionAttributeValues={':incr': 1},
+        ReturnValues="UPDATED_NEW"
+    )
 
-      # Convert Decimal to int before returning
-      visit_count = int(response['Attributes']['visitCount'])  # Convert to int
+    # Convert Decimal to int before returning
+    visit_count = int(response['Attributes']['visitCount'])  # Convert to int
 
-      return {
-                  'statusCode': 200,
-                          'body': json.dumps({'visitCount': visit_count})  # Use the converted visit_count
-                              }
-
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'visitCount': visit_count})  # Use the converted visit_count
+    }
